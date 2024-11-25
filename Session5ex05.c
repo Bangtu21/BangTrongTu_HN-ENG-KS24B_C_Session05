08eb3aed2f7286347d0d4859af5dd29bcfc1ff31
Bangtu21 <bangtu2005.com@gmail.com>
2024-11-26 01:01:33 +0700

    Add files via upload

---
+++ Session5ex05.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+	int number1, number2;
+	for (number1=1; number1<=9; number1++){
+		printf("bang cuu chuong cua %d la: \n", number1);
+	for (number2=1; number2<=10; number2++){
+		printf("%d x %d = %d\n", number1, number2, number1*number2);
+	}
+}
+}
+
