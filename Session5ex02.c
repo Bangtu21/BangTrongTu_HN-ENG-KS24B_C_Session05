3ef8dd3786389b5c1402ae17a5de8d672ea81bc9
Bangtu21 <bangtu2005.com@gmail.com>
2024-11-26 01:00:39 +0700

    Add files via upload

---
+++ Session5ex02.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+	int number;
+	printf("Moi ban nhap 1 so nguyen ");
+	scanf("%d",&number);
+	while(number!=10){
+		printf("Moi ban nhap so nguyen ");
+		scanf("%d",&number);
+	}
+}
+
